75fe1e03f2426626acc634deb1559200e3fba8a5
Pakyudae . <visitacionmaquee@gmail.com>
2026-09-17 13:21:55 +0800

    Initialize live backend URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   useEffect(() => {
     // We will change this URL to your live Render URL after deployment
-    fetch('https://multiplayer-whiteboard-jh3d.onrender.com/api/ping')
+    fetch('https://multiplayer-whiteboard-jh3d.onrender.com')
       .then(response => response.json())
       .then(data => setServerMessage(data.message))
       .catch(error => setServerMessage('Backend connection failed.'));
